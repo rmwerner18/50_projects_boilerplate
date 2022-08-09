305ada91d5d6885f05e97fff0684950b88a6242b
humorous-storage-7521 <rmwerner@umich.edu>
2022-08-09 00:10:05 -0500

    add wave effect

--- input-form-wave/script.js
+++ input-form-wave/script.js
@@ -4,6 +4,6 @@ const labels = document.querySelectorAll('.form-control label')
 labels.forEach(label => {
     label.innerHTML = label.innerText
         .split('')
-        .map((letter, index) => `<span>${letter}</span>`)
+        .map((letter, i) => `<span style='transition-delay: ${i*50}ms'>${letter}</span>`)
         .join('')
 })
