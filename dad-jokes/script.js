5bbb1747cd32729eaf349c2cc29b3307dc2134a9
humorous-storage-7521 <rmwerner@umich.edu>
2022-08-10 21:08:33 -0500

    refactor to async/await

--- dad-jokes/script.js
+++ dad-jokes/script.js
@@ -1,18 +1,34 @@
 const jokeEl = document.getElementById('joke')
 const jokeBtn = document.getElementById('jokeBtn')
 
-const generateJoke = () => {
+
+async function generateJokes() {
     const config = {
         headers: {
             'Accept': 'application/json'
         }
     }
-    fetch('https://icanhazdadjoke.com/', config)
-        .then(res => res.json())
-        .then(data => {
-            jokeEl.innerHTML = data.joke
-        })
+
+    const res = await fetch('https://icanhazdadjoke.com/', config)
+
+    const data = await res.json()
+
+    jokeEl.innerHTML = data.joke
 }
+
+
+// const generateJoke = () => {
+//     const config = {
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com/', config)
+//         .then(res => res.json())
+//         .then(data => {
+//             jokeEl.innerHTML = data.joke
+//         })
+// }
 
 generateJoke()
 
