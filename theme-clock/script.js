12ec87cb7a28f7b4da88f8c73defd01ff3172404
humorous-storage-7521 <rmwerner@umich.edu>
2022-08-24 22:21:17 -0500

    start js

---
+++ theme-clock/script.js
@@ -0,0 +1,7 @@
+const hourEl = document.querySelector('.hour')
+const minuteEl = document.querySelector('.minute')
+const secondEl = document.querySelector('.hour')
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+const toggleEl = document.querySelector('.toggle')
+
