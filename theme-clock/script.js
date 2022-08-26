2d65bce986152c7fe707d9c2a5cd0416ef03fbb6
humorous-storage-7521 <rmwerner@umich.edu>
2022-08-25 22:47:40 -0500

    make clock work

--- theme-clock/script.js
+++ theme-clock/script.js
@@ -1,6 +1,6 @@
 const hourEl = document.querySelector('.hour')
 const minuteEl = document.querySelector('.minute')
-const secondEl = document.querySelector('.hour')
+const secondEl = document.querySelector('.second')
 const timeEl = document.querySelector('.time')
 const dateEl = document.querySelector('.date')
 const toggleEl = document.querySelector('.toggle')
@@ -13,3 +13,29 @@ toggleEl.addEventListener('click', e => {
     html.classList.toggle('dark')
     e.target.innerHTML = html.classList.contains('dark') ? 'Light Mode' : 'Dark Mode'
 })
+
+function setTime() {
+    const time = new Date
+    
+    const month = time.getMonth()
+    const day = time.getDay()
+    const hours = time.getHours()
+    const hoursForClock = hours % 12
+    const minutes = time.getMinutes()
+    const seconds = time.getSeconds()
+
+    console.log(hoursForClock)
+
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+setTime()
+
+setInterval(setTime, 1000)
