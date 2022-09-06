d34245a060b7adea8850b0fcb0e08ea568129fc1
humorous-storage-7521 <rmwerner@umich.edu>
2022-09-06 08:38:41 -1000

    make slide shift work;

--- vertical-slider/script.js
+++ vertical-slider/script.js
@@ -2,7 +2,7 @@ const sliderContainer = document.querySelector('.slider-container')
 const slideRight = document.querySelector('.right-slide') 
 const slideLeft = document.querySelector('.left-slide') 
 const upButton = document.querySelector('.up-button') 
-const downButton = document.querySelector('.donw-button')
+const downButton = document.querySelector('.down-button')
 const slidesLength = slideRight.querySelectorAll('div').length
 
 console.log(slidesLength)
@@ -20,13 +20,16 @@ const changeSlide = (direction) => {
 
     if(direction === 'up') {
         activeSlidesIndex++
-        if(activeSlidesIndex > slidesLength) {
+        if(activeSlidesIndex > slidesLength - 1) {
             activeSlidesIndex = 0
         }
     } else {
         activeSlidesIndex--
         if(activeSlidesIndex < 0) {
-            activeSlidesIndex = slidesLength
+            activeSlidesIndex = slidesLength - 1
         }      
     }
+    slideRight.style.transform = `translateY(-${activeSlidesIndex * sliderHeight}px)`
+    slideLeft.style.transform = `translateY(${activeSlidesIndex * sliderHeight}px)`
+
 }   
