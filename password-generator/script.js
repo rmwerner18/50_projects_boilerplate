8e84a4f59cf26bad083f35c357962f43b5d9bec3
humorous-storage-7521 <rmwerner@umich.edu>
2022-09-16 11:50:40 -0500

    make password generator work

--- password-generator/script.js
+++ password-generator/script.js
@@ -1,8 +1,52 @@
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const lengthEl = document.getElementById('length')
+const resultEl = document.getElementById('result')
+const generateEl = document.getElementById('generate')
+const clipboardEl = document.getElementById('clipboard')
+
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value
+    const uppercase = uppercaseEl.checked
+    const lowercase = lowercaseEl.checked
+    const numbers = numbersEl.checked
+    const symbols = symbolsEl.checked
+
+    resultEl.innerText = generatePassword(uppercase, lowercase, numbers, symbols, length)
+
+})
+
+function generatePassword(uppercase, lowercase, numbers, symbols, length) {
+    let generatedPassword = ''
+    const typesCount = uppercase + lowercase + numbers + symbols
+    const typesArray = [{uppercase}, {lowercase}, {numbers}, {symbols}].filter(item => Object.values(item)[0])
+
+    if (typesCount === 0) {
+        return ''
+    } 
+
+    for (let i = 0; i < length; i++) {
+        let type = Object.keys(typesArray[Math.floor(Math.random() * typesCount)])[0]
+        generatedPassword = generatedPassword + randomFunc[type]()
+    }
+
+    return generatedPassword
+}
+
+const randomFunc = {
+    lowercase: getRandomLower,
+    uppercase: getRandomUpper,
+    numbers: getRandomNumber,
+    symbols: getRandomSymbol
+}
+
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
 
-function getRandomUppoer() {
+function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
 }
 
