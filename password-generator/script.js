1ae686a985bb548ca6c1a322b33d2553292619b9
humorous-storage-7521 <rmwerner@umich.edu>
2022-09-16 11:58:14 -0500

    copy to clipboard

--- password-generator/script.js
+++ password-generator/script.js
@@ -7,6 +7,19 @@ const resultEl = document.getElementById('result')
 const generateEl = document.getElementById('generate')
 const clipboardEl = document.getElementById('clipboard')
 
+clipboardEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea')
+    const password = resultEl.innerText
+
+    if (!password) { return }
+    textarea.value = password
+    document.body.appendChild(textarea)
+    textarea.select()
+    document.execCommand('copy')
+    textarea.remove()
+    alert('Password copied to clipboard')
+})
+
 generateEl.addEventListener('click', () => {
     const length = +lengthEl.value
     const uppercase = uppercaseEl.checked
