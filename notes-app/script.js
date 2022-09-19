f6c2af0d2c4551336d05dc1e605cef0fec2df84e
humorous-storage-7521 <rmwerner@umich.edu>
2022-09-18 20:28:19 -0500

    make delete and edit btns work

--- notes-app/script.js
+++ notes-app/script.js
@@ -1,6 +1,8 @@
 const addBtn = document.getElementById('add')
 
-addBtn.addEventListener('click', () => {
+addBtn.addEventListener('click', )
+
+function addNote(text = '') {
     let note = document.createElement('div')
     note.className = 'note'
     note.innerHTML = `
@@ -9,9 +11,27 @@ addBtn.addEventListener('click', () => {
         <button class="delete"><i class="fas fa-trash-alt"></i></button>
     </div>
 
-    <div class="main"></div>
-    <textarea></textarea>
+    <div class="main ${text ? '' : 'hidden'}"></div>
+    <textarea class="${text ? 'hidden' : ''}"></textarea>
     `
 
-    document.querySelector('body').appendChild(note)
-})
+    const editBtn = note.querySelector('.edit')
+    const deleteBtn = note.querySelector('.delete')
+    const main = note.querySelector('.main')
+    const textArea = note.querySelector('textarea')
+
+    textArea.value = text
+    main.innerHTML = marked(text)
+
+    deleteBtn.addEventListener('click', () => {
+        note.remove()
+    })
+
+    editBtn.addEventListener('click', () => {
+        main.classList.toggle('hidden')
+        textArea.classList.toggle('hidden')
+    })
+
+    // ÷textArea.addEventListener
+    document.body.appendChild(note)
+}
