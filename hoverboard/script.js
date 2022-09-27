71a750e7bc53f387e260d17bd37a15fa715f164b
humorous-storage-7521 <rmwerner@umich.edu>
2022-09-27 10:03:35 -0500

    add box-shadow

--- hoverboard/script.js
+++ hoverboard/script.js
@@ -1,22 +1,20 @@
 const container = document.getElementById('container')
 
-let colors = [
-    'red',
-    'blue',
-    'yellow',
-    'purple',
-    'green',
-    'orange'
-]
+let colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
 
 for (let i = 0; i < 500; i++) {
     let square = document.createElement('div')
     square.className = 'square'
+
     square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = `${colors[Math.floor(Math.random() * 10)]}`
-        setTimeout(() => {
-            square.style.backgroundColor = '#1d1d1d'
-        }, 100)
+        let color = colors[Math.floor(Math.random() * colors.length)]
+        square.style.backgroundColor = `${color}`
+        element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
     })
+    square.addEventListener('mouseout', () => {
+        square.style.backgroundColor = '#1d1d1d'
+        element.style.boxShadow = '0 0 2px #000'
+    })
+    
     container.appendChild(square)
 }
