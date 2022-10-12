b837455b0313f72b65e47513caef9a09e99c73a4
humorous-storage-7521 <rmwerner@umich.edu>
2022-10-11 23:44:34 -0500

    delete console.logs

--- mobile-navigation/script.js
+++ mobile-navigation/script.js
@@ -1,12 +1,8 @@
 const contents = document.querySelectorAll('.content')
 const lis = document.querySelectorAll('li')
 
-console.log(lis)
-console.log(contents)
-
 lis.forEach((li, i) => {
  li.addEventListener('click', () => {
-  console.log('eksdfbhjvadvjn')
   addClassToEl('active', lis, li)
   addClassToEl('show', contents, contents[i])
  })
