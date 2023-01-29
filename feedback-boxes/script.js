71ff6462e4a0e5666e6a399bb85e92a539388ed5
humorous-storage-7521 <ryanwerner50@gmail.com>
2023-01-29 17:12:40 -0600

    finish

--- feedback-boxes/script.js
+++ feedback-boxes/script.js
@@ -1,15 +1,15 @@
 const ratings = document.querySelectorAll('.rating');
 const sendBtn = document.querySelector('#send');
 const panel = document.querySelector('#panel');
-let selectedRating = 'Satisfied'
+let selectedRating = 'Satisfied';
 
 panel.addEventListener('click', (e) => {
   if (e.target.parentNode.classList.contains('rating')) {
     ratings.forEach((rating) => {
-      rating.classList.remove('active')
+      rating.classList.remove('active');
     });
     e.target.parentNode.classList.add('active');
-    selectedRating = e.target.nextElementSibling.innerHTML
+    selectedRating = e.target.nextElementSibling.innerHTML;
   }
   if (e.target.classList.contains('btn')) {
     panel.innerHTML = `
@@ -18,9 +18,6 @@ panel.addEventListener('click', (e) => {
       <br/>
       <strong>Feedback: ${selectedRating}</strong>
       <p>We will use your feedback to improve our customer support.</p>
-    `
-
-
-    
+    `;
   }
 });
